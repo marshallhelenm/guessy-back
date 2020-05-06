8cdd098cf11ff3f8f4bf2ef481aebfeb774d69e8
marshallhelenm <marshallhelenm@gmail.com>
2020-05-06 14:55:00 -0700

    simplified server and got it running

---
+++ memeCollection.js
@@ -0,0 +1,16 @@
+const memes = [
+    {
+        id: 1,
+        stub: "sure_hope",
+        title: "I sure hope it does",
+        img: 'https://res.cloudinary.com/dwfqeeh5f/image/upload/v1588045049/Guessy/memes/i_sure_hope.jpg'
+    },
+    {
+        id: 2,
+        stub: "is_pigeon",
+        title: "Is this a pigeon?",
+        img: "https://res.cloudinary.com/dwfqeeh5f/image/upload/v1588044932/Guessy/memes/is_this_a_pigeon.jpg"
+    }
+]
+
+module.exports = memes;
