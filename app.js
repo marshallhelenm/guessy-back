55975509f09031d97f0743dd4d68c630afde446a
marshallhelenm <marshallhelenm@gmail.com>
2020-05-19 12:56:22 -0700

    need to adapt the server so that express and socket arent clashing

--- app.js
+++ app.js
@@ -4,13 +4,22 @@ const socketio = require("socket.io");
 
 const app = express();
 const server = http.createServer(app);
-const io = socketio(server);
+const io = socketio(server, { origins: "*:*" });
 const router = require("./router");
+const cors = require("cors");
 
 const port = process.env.PORT || 5000;
 const memes = require("./memeCollection");
 
 app.use(router);
+app.use(cors())
+
+io.origins((origin, callback) => {
+  if (origin !== 'https://foo.example.com') {
+    return callback('origin not allowed', false);
+  }
+  callback(null, true);
+});
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
@@ -34,7 +43,7 @@ app.listen(port, () => {
 
 io.on("connection", (socket) => {
   console.log("new connection");
-  socket.on('disconnect', ()=>{
-      console.log('user left')
-  })
+  socket.on("disconnect", () => {
+    console.log("user left");
+  });
 });
--- memeCollection.js
+++ memeCollection.js
@@ -1,10 +1,11 @@
 const memes = [
   {
-    id: "1_sure_hope",
-    stub: "sure_hope",
-    title: "I sure hope it does",
+    id: "1_road_work",
+    stub: "road_work",
+    title: "Road Work Ahead",
     img:
       "https://res.cloudinary.com/dwfqeeh5f/image/upload/v1588045049/Guessy/memes/i_sure_hope.jpg",
+    origin: "https://knowyourmeme.com/memes/road-work-ahead",
   },
   {
     id: "2_is_pigeon",
@@ -12,6 +13,7 @@ const memes = [
     title: "Is this a pigeon?",
     img:
       "https://res.cloudinary.com/dwfqeeh5f/image/upload/v1588044932/Guessy/memes/is_this_a_pigeon.jpg",
+    origin: "https://knowyourmeme.com/memes/is-this-a-pigeon",
   },
   {
     id: "3_pikachu",
@@ -19,48 +21,55 @@ const memes = [
     title: "Shocked Pikachu",
     img:
       "https://res.cloudinary.com/dwfqeeh5f/image/upload/v1588959076/Guessy/memes/image0.png",
+    origin: "https://knowyourmeme.com/memes/surprised-pikachu",
   },
   {
-    id: "1_sure_hope",
-    stub: "sure_hope",
-    title: "I sure hope it does",
+    id: "4_then_perish",
+    stub: "then_perish",
+    title: "Then Perish",
     img:
-      "https://res.cloudinary.com/dwfqeeh5f/image/upload/v1588045049/Guessy/memes/i_sure_hope.jpg",
+      "https://res.cloudinary.com/dwfqeeh5f/image/upload/v1589822516/Guessy/memes/perish.jpg",
+    origin: "https://knowyourmeme.com/memes/then-perish",
   },
   {
-    id: "2_is_pigeon",
-    stub: "is_pigeon",
-    title: "Is this a pigeon?",
+    id: "5_real_estate",
+    stub: "real_estate",
+    title: "It's Free Real Estate",
     img:
-      "https://res.cloudinary.com/dwfqeeh5f/image/upload/v1588044932/Guessy/memes/is_this_a_pigeon.jpg",
+      "https://res.cloudinary.com/dwfqeeh5f/image/upload/v1589828289/Guessy/memes/download_1.jpg",
+    origin: "https://knowyourmeme.com/memes/its-free-real-estate",
   },
   {
-    id: "3_pikachu",
-    stub: "pikachu",
-    title: "Shocked Pikachu",
+    id: "6_woman_yelling_cat",
+    stub: "woman_yelling_cat",
+    title: "Woman Yelling At Cat",
     img:
-      "https://res.cloudinary.com/dwfqeeh5f/image/upload/v1588959076/Guessy/memes/image0.png",
+      "https://res.cloudinary.com/dwfqeeh5f/image/upload/v1589844095/Guessy/memes/womanyellingcat.jpg",
+    origin: "https://knowyourmeme.com/memes/woman-yelling-at-a-cat",
   },
   {
-    id: "1_sure_hope",
-    stub: "sure_hope",
-    title: "I sure hope it does",
+    id: "7_this_is_fine",
+    stub: "this_is_fine",
+    title: "This Is Fine",
     img:
-      "https://res.cloudinary.com/dwfqeeh5f/image/upload/v1588045049/Guessy/memes/i_sure_hope.jpg",
+      "https://res.cloudinary.com/dwfqeeh5f/image/upload/v1589850523/Guessy/memes/Screen_Shot_2020-05-18_at_6.09.39_PM.png",
+    origin: "https://knowyourmeme.com/memes/this-is-fine",
   },
   {
-    id: "2_is_pigeon",
-    stub: "is_pigeon",
-    title: "Is this a pigeon?",
+    id: "8_evil_kermit",
+    stub: "evil_kermit",
+    title: "Evil Kermit",
     img:
-      "https://res.cloudinary.com/dwfqeeh5f/image/upload/v1588044932/Guessy/memes/is_this_a_pigeon.jpg",
+      "https://res.cloudinary.com/dwfqeeh5f/image/upload/v1589852447/Guessy/memes/evil_kermit.jpg",
+    origin: "https://knowyourmeme.com/memes/evil-kermit",
   },
   {
-    id: "3_pikachu",
-    stub: "pikachu",
-    title: "Shocked Pikachu",
+    id: "9_steel_feathers",
+    stub: "steel_feathers",
+    title: "But Steel is Heavier Than Feathers...",
     img:
-      "https://res.cloudinary.com/dwfqeeh5f/image/upload/v1588959076/Guessy/memes/image0.png",
+      "https://res.cloudinary.com/dwfqeeh5f/image/upload/v1589852807/Guessy/memes/steel_feathers.png",
+    origin: "https://knowyourmeme.com/memes/steel-is-heavier-than-feathers",
   },
 ];
 
